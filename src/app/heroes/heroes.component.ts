77f42e07258d62539bc614b3d0556642bd5be3d3
RJonesIII <Rnj4701@rit.edu>
2025-09-26 23:11:50 -0400

    Starting on the last part

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { NgFor } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
+  // standalone: true,
+  // imports: [
+  //   NgFor,
+  // ]
 })
 export class HeroesComponent {
+
+  
   
   //hero = 'Windstorm';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+  heroes = HEROES;
 
 }
